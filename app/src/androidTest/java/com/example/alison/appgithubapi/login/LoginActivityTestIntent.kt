c3a26a5b97c45e713eb20bf9a7c63d0bebb40865
Alison Soldado <alison.soldadoo@gmail.com>
2018-03-05 17:34:47 -0300

    Altera código para o teste não abrir o repository activity

--- app/src/androidTest/java/com/example/alison/appgithubapi/login/LoginActivityTestIntent.kt
+++ app/src/androidTest/java/com/example/alison/appgithubapi/login/LoginActivityTestIntent.kt
@@ -1,8 +1,11 @@
 package com.example.alison.appgithubapi.login
 
+import android.app.Activity
+import android.app.Instrumentation
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.intent.Intents.intended
+import android.support.test.espresso.intent.Intents.intending
 import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import android.support.test.espresso.intent.rule.IntentsTestRule
 import android.support.test.espresso.matcher.ViewMatchers.withId
@@ -31,7 +34,11 @@ class LoginActivityTestIntent {
     fun givenEmailAndPasswordCorrect_WhenClickOnLoginButton_ThenShouldShootIntent() {
         onView(withId(R.id.edtEmailLogin)).perform(typeText(EMAIL), closeSoftKeyboard())
         onView(withId(R.id.edtPasswordLogin)).perform(typeText(PASSWORD), closeSoftKeyboard())
+        val matcher = hasComponent(RepositoryActivity::class.java.name)
+        val activityResult = Instrumentation.ActivityResult(Activity.RESULT_OK, null)
+        intending(matcher).respondWith(activityResult)
         onView(withId(R.id.btnEnter)).perform(click())
-        intended(hasComponent(RepositoryActivity::class.java.name))
+        intended(matcher)
     }
+
 }
